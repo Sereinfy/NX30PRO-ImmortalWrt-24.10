e0a0d2283c0811eb0b2d2c547a1cba7a45ebb83b
Sereinfy <122258839+Sereinfy@users.noreply.github.com>
2025-07-08 10:45:44 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,18 +12,18 @@
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
 # mosdns
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
-rm -rf feeds/packages/net/mosdns
-rm -rf feeds/packages/net/v2ray-geodata
-rm -rf package/feeds/packages/mosdns
-rm -rf package/feeds/packages/v2ray-geodata
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+#rm -rf feeds/packages/lang/golang
+#git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+#rm -rf feeds/packages/net/mosdns
+#rm -rf feeds/packages/net/v2ray-geodata
+#rm -rf package/feeds/packages/mosdns
+#rm -rf package/feeds/packages/v2ray-geodata
+#git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+#git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 #git clone https://github.com/nikkinikki-org/OpenWrt-nikki.git package/luci-app-nikki
 
-git clone https://github.com/sirpdboy/luci-app-wizard package/luci-app-wizard
+#git clone https://github.com/sirpdboy/luci-app-wizard package/luci-app-wizard
 
-rm -rf package/feeds/packages/openclash
-rm -rf feeds/packages/net/openclash
-git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+#rm -rf package/feeds/packages/openclash
+#rm -rf feeds/packages/net/openclash
+#git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
