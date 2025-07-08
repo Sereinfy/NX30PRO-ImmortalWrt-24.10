424ed8be492a4e901df282409335cb62c452e6b9
Sereinfy <122258839+Sereinfy@users.noreply.github.com>
2025-07-08 15:10:13 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,21 +9,26 @@
 #============================================================
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+# 默认网关 ip 地址修改
+sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/luci2/bin/config_generate
+
 sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
 # mosdns
-#rm -rf feeds/packages/lang/golang
-#git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
-#rm -rf feeds/packages/net/mosdns
-#rm -rf feeds/packages/net/v2ray-geodata
-#rm -rf package/feeds/packages/mosdns
-#rm -rf package/feeds/packages/v2ray-geodata
-#git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-#git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-#git clone https://github.com/nikkinikki-org/OpenWrt-nikki.git package/luci-app-nikki
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+rm -rf feeds/packages/net/mosdns
+rm -rf feeds/packages/net/v2ray-geodata
+rm -rf package/feeds/packages/mosdns
+rm -rf package/feeds/packages/v2ray-geodata
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
-#git clone https://github.com/sirpdboy/luci-app-wizard package/luci-app-wizard
+git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 
-#rm -rf package/feeds/packages/openclash
-#rm -rf feeds/packages/net/openclash
+rm -rf feeds/luci/applications/luci-app-wizard
+git clone https://github.com/sirpdboy/luci-app-wizard package/luci-app-wizard
+
+
+rm -rf feeds/luci/applications/luci-app-openclash
 #git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
